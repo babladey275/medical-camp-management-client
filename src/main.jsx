ffa40dc4086775c491ffd2d755c3d2ed82e46440
babladey275 <babladey275@gmail.com>
2025-01-20 20:36:51 +0530

    added authContext provider with user authentication methods

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router.jsx";
+import AuthProvider from "./providers/AuthProvider/AuthProvider.jsx";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+    <Toaster />
   </StrictMode>
 );
