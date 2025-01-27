db84ffc285970e6283043bc44141b469a50afc4f
babladey275 <babladey275@gmail.com>
2025-01-27 17:24:05 +0530

    added admin hook and admin route

--- src/pages/Dashboard/SideNavbar/SideNavbar.jsx
+++ src/pages/Dashboard/SideNavbar/SideNavbar.jsx
@@ -8,93 +8,97 @@ import {
   FaHistory,
   FaUserTie,
 } from "react-icons/fa";
+import useAdmin from "../../../hooks/useAdmin";
 
 const SideNavbar = () => {
+  const [isAdmin] = useAdmin();
+
   return (
     <div className="w-64 min-h-screen bg-gray-800 text-white p-4">
-      {/* Organizer Section */}
       <ul className="space-y-4">
-        <li>
-          <Link
-            to="/organizer-profile"
-            className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700"
-          >
-            <FaUserTie className="text-xl" />
-            <span>Organizer Profile</span>
-          </Link>
-        </li>
-        <li>
-          <NavLink
-            to="/dashboard/add-camp"
-            className={({ isActive }) =>
-              isActive
-                ? "bg-white text-gray-700 p-2 font-bold flex items-center space-x-2 rounded-lg"
-                : "flex space-x-2 items-center p-2 rounded-lg hover:bg-gray-700"
-            }
-          >
-            <FaPlusCircle className="text-xl" />
-            <span>Add A Camp</span>
-          </NavLink>
-        </li>
-        <li>
-          <Link
-            to="/manage-camps"
-            className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700"
-          >
-            <FaListAlt className="text-xl" />
-            <span>Manage Camps</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/manage-registered-camps"
-            className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700"
-          >
-            <FaRegistered className="text-xl" />
-            <span>Manage Registered Camps</span>
-          </Link>
-        </li>
-      </ul>
-
-      {/* Participant Section */}
-      <ul className="ml-4 space-y-4">
-        <li>
-          <Link
-            to="/analytics"
-            className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700"
-          >
-            <FaChartBar className="text-xl" />
-            <span>Analytics</span>
-          </Link>
-        </li>
-
-        <li>
-          <Link
-            to="/participant-profile"
-            className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700"
-          >
-            <FaUserAlt className="text-xl" />
-            <span>Participant Profile</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/registered-camps"
-            className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700"
-          >
-            <FaListAlt className="text-xl" />
-            <span>Registered Camps</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/payment-history"
-            className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700"
-          >
-            <FaHistory className="text-xl" />
-            <span>Payment History</span>
-          </Link>
-        </li>
+        {isAdmin ? (
+          <>
+            <li>
+              <Link
+                to="/organizer-profile"
+                className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700"
+              >
+                <FaUserTie className="text-xl" />
+                <span>Organizer Profile</span>
+              </Link>
+            </li>
+            <li>
+              <NavLink
+                to="/dashboard/add-camp"
+                className={({ isActive }) =>
+                  isActive
+                    ? "bg-white text-gray-700 p-2 font-bold flex items-center space-x-2 rounded-lg"
+                    : "flex space-x-2 items-center p-2 rounded-lg hover:bg-gray-700"
+                }
+              >
+                <FaPlusCircle className="text-xl" />
+                <span>Add A Camp</span>
+              </NavLink>
+            </li>
+            <li>
+              <Link
+                to="/manage-camps"
+                className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700"
+              >
+                <FaListAlt className="text-xl" />
+                <span>Manage Camps</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/manage-registered-camps"
+                className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700"
+              >
+                <FaRegistered className="text-xl" />
+                <span>Manage Registered Camps</span>
+              </Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link
+                to="/analytics"
+                className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700"
+              >
+                <FaChartBar className="text-xl" />
+                <span>Analytics</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/participant-profile"
+                className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700"
+              >
+                <FaUserAlt className="text-xl" />
+                <span>Participant Profile</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/registered-camps"
+                className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700"
+              >
+                <FaListAlt className="text-xl" />
+                <span>Registered Camps</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/payment-history"
+                className="flex items-center space-x-2 p-2 rounded-lg hover:bg-gray-700"
+              >
+                <FaHistory className="text-xl" />
+                <span>Payment History</span>
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
---
+++ src/router/AdminRoute.jsx
@@ -0,0 +1,25 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../hooks/useAdmin";
+import useAuth from "../hooks/useAuth";
+
+const AdminRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const [isAdmin, isAdminLoading] = useAdmin();
+  const location = useLocation();
+
+  if (loading || isAdminLoading) {
+    return (
+      <div className="min-h-screen flex justify-center items-center">
+        <span className="loading loading-ring loading-lg"></span>
+      </div>
+    );
+  }
+
+  if (user && isAdmin) {
+    return children;
+  }
+
+  return <Navigate to={"/"} state={{ from: location }} replace></Navigate>;
+};
+
+export default AdminRoute;
