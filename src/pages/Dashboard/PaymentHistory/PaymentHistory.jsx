9b32039acc9d432c885e28e32d6c21b798d3eb80
babladey275 <babladey275@gmail.com>
2025-01-31 17:27:09 +0530

    added payment history

--- src/pages/Dashboard/PaymentHistory/PaymentHistory.jsx
+++ src/pages/Dashboard/PaymentHistory/PaymentHistory.jsx
@@ -1,7 +1,47 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+
 const PaymentHistory = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: payments = [] } = useQuery({
+    queryKey: ["payments", user.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/payments/${user.email}`);
+      return res.data;
+    },
+  });
   return (
     <div>
-      <h2 className="text-3xl text-center font-semibold">Payment History</h2>
+      <h2 className="text-3xl font-semibold text-center mb-8">
+        Payment History
+      </h2>
+      <div className="overflow-x-auto rounded-t-lg">
+        <table className="table">
+          <thead className="bg-[#399ced] text-white">
+            <tr className="text-sm">
+              <th>#</th>
+              <th>Camp Name</th>
+              <th>Camp Fees</th>
+              <th>Payment Status</th>
+              <th>Transaction Id</th>
+            </tr>
+          </thead>
+          <tbody>
+            {payments.map((item, index) => (
+              <tr key={item._id}>
+                <td>{index + 1}</td>
+                <td>{item.campName}</td>
+                <td>${item.fees}</td>
+                <td>{item.paymentStatus}</td>
+                <td>{item.transactionId}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
